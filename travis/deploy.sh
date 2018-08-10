9bc66d61746ddaf5d6e345eec0e1a64e364a2b7b
ankitjain28may <ankitjain28may77@gmail.com>
2018-08-11 02:42:54 +0530

    Fixed

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -7,8 +7,8 @@ cd /var/www/html/affiliates_connect_demo
 echo "Pulling changes from Git"
 sudo git pull origin dev
 echo "Running Composer Install"
-docker exec -it affiliates_connect composer install
+docker exec affiliates_connect composer install
 echo "Running update entities"
-docker exec -it affiliates_connect vendor/bin/drupal upe
+docker exec affiliates_connect vendor/bin/drupal upe
 echo "Running Cache rebuild"
-docker exec -it affiliates_connect vendor/bin/drupal cr
+docker exec affiliates_connect vendor/bin/drupal cr
